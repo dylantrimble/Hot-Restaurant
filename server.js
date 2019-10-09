263ee34e1d51663d2c726923f5c01f4cf43c29f7
dyl trimble <dyltrimble12@gmail.com>
2019-10-09 19:33:44 -0400

    started working on the server

--- server.js
+++ server.js
@@ -16,25 +16,10 @@ app.use(express.json());
 // =============================================================
 var reservations = [
   {
-    routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
-  },
-  {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
-  },
-  {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
+    customerName: "Mirko's Family",
+    phoneNumber: "7048024004",
+    customerEmail: "random@gmail.com",
+    customerID: "#69696969696"
   }
 ];
 
